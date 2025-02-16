a8009673b06a2c7e0a35b4ee2aef6b0087ebbb8a
amirmejri96 <amirmejri1996@gmail.com>
2025-02-16 03:03:07 +0100

    bouton boutique

--- ShoesVap---Frontend/src/pages/Boutique/Boutique.js
+++ ShoesVap---Frontend/src/pages/Boutique/Boutique.js
@@ -9,41 +9,46 @@ const Boutique = () => {
                 <div className="boxs">
                     <img src="../../../assets/SHAMPO.jpg" alt="Produit 2" />
                     <div className="description">
-                        <a href='/shampoo'><h3>Shampoo Shoesvap</h3></a>
+                        <h3>Shampoo Shoesvap</h3>
                         <p>Quantité : 150ml</p>
                         <div className="price"><span style={{textDecoration:"line-through", marginRight:'10px'}}>20 TND</span><span>15 TND</span></div>
+                        <a href='/shampoo'><button>Plus d'informations</button></a>
                     </div>
                 </div>
                 <div className="boxs">
                     <img src="../../../assets/FOAM.jpg" alt="Produit 1" />
                     <div className="description">
-                        <a href='/foam'><h3>Foam Shoesvap</h3></a>
+                        <h3>Foam Shoesvap</h3>
                         <p>Quantité : 150ml</p>
                         <div className="price"><span style={{textDecoration:"line-through", marginRight:'10px'}}>20 TND</span><span>15 TND</span></div>
+                        <a href='/foam'><button>Plus d'informations</button></a>
                     </div>
                 </div>
                 <div className="boxs">
                     <img src="../../../assets/WHITE.jpg" alt="Produit 3" />
                     <div className="description">
-                        <a href='/white-renew'><h3>White Renew Shoesvap</h3></a>
+                        <h3>White Renew Shoesvap</h3>
                         <p>Quantité : 100ml</p>
                         <div className="price"><span style={{textDecoration:"line-through", marginRight:'10px'}}>20 TND</span><span>15 TND</span></div>
+                        <a href='/white-renew'><button>Plus d'informations</button></a>
                     </div>
                 </div>
                 <div className="boxs">
                     <img src="../../../assets/DUO.jpg" alt="Produit 4" />
                     <div className="description">
-                        <a href='/sneakers-deo'><h3>Sneakers Deo Shoesvap</h3></a>
+                        <h3>Sneakers Deo Shoesvap</h3>
                         <p>Quantité : 250ml</p>
                         <div className="price"><span style={{textDecoration:"line-through", marginRight:'10px'}}>20 TND</span><span>15 TND</span></div>
+                        <a href='/sneakers-deo'><button>Plus d'informations</button></a>
                     </div>
                 </div>
                 <div className="boxs">
                     <img src="../../../assets/PACK NEW.jpg" alt="Produit 4" />
                     <div className="description">
-                        <a href='/pack'><h3>Pack Total Care Shoesvap</h3></a>
+                        <h3>Pack Total Care Shoesvap</h3>
                         <p>Tout ce dont vous avez besoin.</p>
                         <div className="price"><span style={{textDecoration:"line-through", marginRight:'10px'}}>49 TND</span><span>39 TND</span></div>
+                        <a href='/pack'><button>Plus d'informations</button></a>
                     </div>
                 </div>
             </div>
